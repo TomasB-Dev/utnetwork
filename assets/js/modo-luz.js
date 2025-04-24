e07666149d85d0a31c9f7729ad5e01e01e7725c1
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-04-23 21:30:04 -0300

    Update modo-luz.js

--- assets/js/modo-luz.js
+++ assets/js/modo-luz.js
@@ -1,4 +1,3 @@
-const MODO_FINAL = localStorage.getItem('modo')
 const modo = localStorage.getItem('modo');
 const fondo = document.getElementById('body');
 const btn = document.getElementById('swap-btn');
@@ -13,7 +12,6 @@ el resto de cosas que cambia es el boton para la luna y el sol que lo pueden rob
 if (modo == 'claro'){
     btn.style.backgroundColor = '#282828';
         fondo.style.background = '#f8f8ff';   
-        localStorage.setItem("modo", "claro"); 
         btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 12.79A9 9 0 0111.21 3a7 7 0 100 14A9 9 0 0021 12.79z" fill="currentColor"/></svg>';
         btn.style.color = 'white';
         for(const element of txt) {
@@ -33,24 +31,35 @@ if (modo == 'claro'){
 
 
 function changue_BG() {
-    cant_textos = formulario.length;
     if (modo != 'claro') {
-        btn.style.backgroundColor = '#282828';
-        fondo.style.background = '#f8f8ff';   
-        localStorage.setItem("modo", "claro"); 
-        btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 12.79A9 9 0 0111.21 3a7 7 0 100 14A9 9 0 0021 12.79z" fill="currentColor"/></svg>';
-        btn.style.color = 'white';
-        for(const element of txt) {
-            element.style.color = 'black';
-        }
+        try {
+            btn.style.backgroundColor = '#282828';
+            fondo.style.background = '#f8f8ff';   
+            localStorage.setItem("modo", "claro"); 
+            btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 12.79A9 9 0 0111.21 3a7 7 0 100 14A9 9 0 0021 12.79z" fill="currentColor"/></svg>';
+            btn.style.color = 'white';
+            for(const element of txt) {
+                element.style.color = 'black';
+            }
+            console.log('entro claro')
+            } catch (error) {
+                console.log(error)
+            }       
     }else{
-        btn.style.backgroundColor = 'white';
-        btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="5" fill="currentColor"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42" stroke="currentColor" stroke-width="2" stroke-linecap="round"/></svg>';
-        btn.style.color = "#282828"
-        fondo.style.background = '#282828';   
-        for(const element of txt) { 
-            element.style.color = 'white';
+        try {
+            btn.style.backgroundColor = 'white';
+            btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="5" fill="currentColor"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42" stroke="currentColor" stroke-width="2" stroke-linecap="round"/></svg>';
+            btn.style.color = "#282828"
+            fondo.style.background = '#282828';   
+            for(const element of txt) { 
+                element.style.color = 'white';
+            }
+            console.log('entro oscuro ')
+            localStorage.setItem("modo", "oscuro"); 
+        } catch (error) {
+
+            console.log(error)
         }
-        localStorage.setItem("modo", "oscuro"); 
+        
     }
 }
