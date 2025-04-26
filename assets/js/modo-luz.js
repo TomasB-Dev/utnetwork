e02f3627ae13185513975970316d67df48454ed4
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-04-26 17:37:13 -0300

    add class "text" to texts cuz that can be called for js script

--- assets/js/modo-luz.js
+++ assets/js/modo-luz.js
@@ -37,12 +37,18 @@ switch (page_calling) {
                 btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 12.79A9 9 0 0111.21 3a7 7 0 100 14A9 9 0 0021 12.79z" fill="currentColor"/></svg>';
                 btn.style.color = 'white';
                 fondo.style.color = "black"
+                for(const element of txt) { 
+            
+                    element.style.color = 'black';
+                }
         }else{
             btn.style.backgroundColor = 'white';
             btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="5" fill="currentColor"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42" stroke="currentColor" stroke-width="2" stroke-linecap="round"/></svg>';
             btn.style.color = "#282828"
             fondo.style.background = '#282828';
-            
+            for(const element of txt) {
+                element.style.color = 'white';
+            }
             
         }
     default:
