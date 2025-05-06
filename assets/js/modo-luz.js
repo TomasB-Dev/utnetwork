5164a071fa429833fb91d7453855c0fd3c0b7040
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-05-06 17:09:08 -0300

    Update modo-luz.js

--- assets/js/modo-luz.js
+++ assets/js/modo-luz.js
@@ -32,6 +32,8 @@ switch (page_calling) {
         break;
     case 'index.html':
         if (modo == 'claro'){
+                // const estadistica = document.getElementsByClassName('container_statistics')
+                // estadistica.style.color = "black"
                 btn.style.backgroundColor = '#282828';
                 fondo.style.background = '#f8f8ff'; 
                 btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 12.79A9 9 0 0111.21 3a7 7 0 100 14A9 9 0 0021 12.79z" fill="currentColor"/></svg>';
