f27c1394ad4ef2a109b9ab9cf6022d15685b7f27
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-05-07 16:09:26 -0300

    fix compare

---
+++ assets/js/repeat-key.js
@@ -0,0 +1,13 @@
+const key1 = document.getElementById('password')
+const key2 = document.getElementById('repassword')
+const estado = document.getElementById('igual')
+
+key2.addEventListener("input",check)
+function check() {
+    if( key2===key1){
+        estado.textContent = "✅"
+    }
+    else{
+        estado.textContent ="❌"
+    }
+}
