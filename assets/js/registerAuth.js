9fdf952db5eb07c37f7c60bf03e3e84d983ee1bc
Agus-Sosa <sosaagustin351@gmail.com>
2025-05-21 17:40:14 -0300

    auth form register

---
+++ assets/js/registerAuth.js
@@ -0,0 +1,151 @@
+
+const authForm= document.getElementById('form');
+
+
+
+
+
+const validateTerm =()=> {
+    const error_message = document.getElementById('error_term')
+    const value = document.getElementById('check')
+
+    if(!value.checked) {
+        error_message.style.display = 'block'
+        error_message.textContent= 'Debes aceptar los terminos y condiciones'
+
+
+        return false
+    } else {
+        error_message.style.display = 'none'
+        value.classList.remove('error_input')
+        return true
+
+    }
+
+
+
+}
+
+
+const validateName = ()=> {
+    const error_message = document.getElementById('error_name');
+    const value = document.getElementById('name').value.trim();
+    const input = document.getElementById('name');
+    const regex = /^[a-zA-Z]+$/;
+
+    let verify = regex.test(value);
+
+
+    if(value.length <3 || !verify) {
+        error_message.textContent= 'El nombre debe tener al menos 3 caracteres y solo letras'
+        error_message.style.display = 'block'
+        input.classList.add('error_input')
+        return false
+    } else {
+        error_message.style.display = 'none'
+        input.classList.remove('error_input')
+        return true;
+    }
+}
+
+
+const validateEmail = ()=> {
+    const error_message = document.getElementById("error_email");
+    const value = document.getElementById('email').value.trim();
+    const input = document.getElementById('email');
+    const regex  = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    let verify = regex.test(value);
+
+
+    if(!verify) {
+        error_message.style.display = 'block'
+        error_message.textContent= 'El correo no es valido'
+        input.classList.add('error_input')
+        return false
+    } else {
+        error_message.style.display = 'none'
+        input.classList.remove('error_input')
+        return true;
+    }
+
+
+}
+
+
+
+const validatePassword = ()=> {
+    const error_message = document.getElementById('error_password');
+    const value = document.getElementById('password').value.trim();
+    const input = document.getElementById('password');
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    let verify = regex.test(value);
+    if(value.length < 8 || !verify) {
+        error_message.textContent= 'La contraseña debe tener al menos 8 caracteres, una letra y un numero'
+        error_message.style.display = 'block'
+        input.classList.add('error_input')
+        return false;
+    } else {
+        error_message.style.display = 'none'
+        input.classList.remove('error_input')
+        return true;
+    }
+
+
+}
+
+
+
+const validateConfirmPassword = ()=> {
+    const error_message = document.getElementById('error_confirm_password');
+    const value = document.getElementById('confirm_password').value.trim();
+    const input = document.getElementById('confirm_password');
+    const password = document.getElementById('password').value.trim();
+
+    if(value !== password) {
+        error_message.style.display = 'block'
+
+        error_message.textContent= 'Las contraseñas no coinciden'
+        input.classList.add('error_input')
+        return false
+    } else {
+        error_message.style.display = 'none'
+        input.classList.remove('error_input')
+            return true;
+
+    }
+
+
+}
+
+
+const successMessage = ()=> {
+    const message = document.getElementById('success_message');
+    message.style.display = 'block';
+    message.textContent = 'Formulario enviado con exito';
+    console.log('Formulario enviado con exito');
+}
+
+
+
+const validateForm = (e)=> {
+    e.preventDefault();
+    const term = validateTerm();
+    const name = validateName();
+    const email = validateEmail();
+    const password = validatePassword();
+    const confirm_password = validateConfirmPassword();
+    
+    if(term && name && email && password && confirm_password){
+        successMessage();
+        console.log('Formulario enviado con exito');
+    } else {
+        return 
+    }
+    
+}
+
+
+
+authForm.addEventListener('submit', validateForm);
