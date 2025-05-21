0ee6dd1ae0d2cfcb79a35026b7d0f5ecce492cdf
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-05-21 16:30:12 -0300

    add login fixed

--- assets/js/login-fake.js
+++ assets/js/login-fake.js
@@ -1,17 +1,26 @@
 document.getElementById('login-form').addEventListener('submit', function (e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    const email = document.getElementById('mail').value.trim();
-    const password = document.getElementById('key').value.trim();
-    const mensaje = document.getElementById('login-msg');
+    const email = document.getElementById('mail').value.trim()
+    const password = document.getElementById('key').value.trim()
+    const mensaje = document.getElementById('login-msg')
 
-    if (email == "mailfake@mail.com" && password =="123456") {
-        mensaje.style.color = 'green';
-        mensaje.textContent = 'Logeadp';
-        
-    
-    } else {
+    if (!validarmail(email)) {
         mensaje.style.color = 'red';
-        mensaje.textContent = 'datos incorrectos, vuelve a intentarlo.';
+        mensaje.textContent = 'correo invalido.'
+        return;
+    }
+
+    if (email == "mailfake@mail.com" && password == "123456") {
+        mensaje.style.color = 'green'
+        mensaje.textContent = 'Logeado'
+    } else {
+        mensaje.style.color = 'red'
+        mensaje.textContent = 'datos incorrectos, vuelve a intentarlo.'
     }
 });
+
+function validarmail(email) {
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return regex.test(email)
+}
