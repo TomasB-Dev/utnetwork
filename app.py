6cd915b7dd55d229d6ddcabbd26d0e0bbabfde5a
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-06-13 10:51:08 -0300

    fix some url

--- app.py
+++ app.py
@@ -109,9 +109,12 @@ def home():
     token = session.get('usuario')
     if session:
         id_user = token[0]['id']
-        user_data = usuarios.get_data_by_id(id_user)
-        state = user_data[0]['state']
-        return render_template('home.html', usuario=user_data[0])
+        info_user = usuarios.get_data_by_id(id_user)
+        user_state = usuarios.get_state_by_id(id_user)
+        if user_state == True:
+            return render_template('home.html', usuario=info_user[0])
+        else:
+            return redirect(url_for('validation'))
     else:
         return render_template('error.html')
 
