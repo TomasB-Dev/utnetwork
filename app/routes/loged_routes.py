8f76a088138511c7d04ac7789475833402c940b2
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-06-18 10:35:50 -0300

    add details with funny questions

--- app/routes/loged_routes.py
+++ app/routes/loged_routes.py
@@ -3,6 +3,8 @@ CONTIENE LAS RUTAS DE CUANDO EL USUARIO YA ESTA LOGUEADO
 """
 from flask import render_template, url_for, request,  redirect , session
 from app.utils.Error_Saver import save_error
+from app.utils.preguntas import preguntas_graciosas
+import random
 
 def logued_route(app, usuarios, publicaciones):
     @app.route('/home')
@@ -13,8 +15,10 @@ def logued_route(app, usuarios, publicaciones):
             info_user = usuarios.get_data_by_id(id_user)
             user_state = usuarios.get_state_by_id(id_user)
             if user_state == True:
+                
+                pregunta = random.choice(preguntas_graciosas)
                 publicacion = publicaciones.ver_publicaciones(id_user) #traigo las publicaciones
-                return render_template('home.html', usuario=info_user[0],publicaciones=publicacion)
+                return render_template('home.html', usuario=info_user[0],publicaciones=publicacion,pregunta=pregunta)
             else:
                 return redirect(url_for('validation'))
         else:
