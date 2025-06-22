414d7c7e7df92e3359a95e2400246c8aaec813f2
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-06-22 12:31:38 -0300

    Update modo-luz.js

--- static/js/modo-luz.js
+++ static/js/modo-luz.js
@@ -84,7 +84,17 @@ switch (page_calling) {
             
                     element.style.color = 'black';
                 }
+        }else{
+            btn.style.backgroundColor = 'white';
+            btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="5" fill="currentColor"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42" stroke="currentColor" stroke-width="2" stroke-linecap="round"/></svg>';
+            btn.style.color = "#282828"
+            fondo.style.background = '##212121';
+            for(const element of txt) {
+                element.style.color = 'white';
+            }
+            
         }
+        
     default:
         break;
 }
@@ -110,8 +120,8 @@ function changue_BG() {
         try {
             btn.style.backgroundColor = 'white';
             btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="5" fill="currentColor"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42" stroke="currentColor" stroke-width="2" stroke-linecap="round"/></svg>';
-            btn.style.color = "#282828"
-            fondo.style.background = '#282828';   
+            btn.style.color = "#212121"
+            fondo.style.background = '#212121';   
             localStorage.setItem("modo", "oscuro");
             for(const element of txt) { 
             
