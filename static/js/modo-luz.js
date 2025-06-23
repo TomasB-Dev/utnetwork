6f8aedc2cae490bb11fc3ca5224da1f077727853
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-06-23 11:10:42 -0300

    fix d/l mode in search

--- static/js/modo-luz.js
+++ static/js/modo-luz.js
@@ -97,6 +97,32 @@ switch (page_calling) {
             fondo.classList.remove('light_mode');  // agrego clase
 
         }
+
+    case 'search.html':
+        if (modo == 'claro'){
+            btn.style.backgroundColor = '#282828';
+                fondo.style.background = '#f8f8ff'; 
+                btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 12.79A9 9 0 0111.21 3a7 7 0 100 14A9 9 0 0021 12.79z" fill="currentColor"/></svg>';
+                btn.style.color = 'white';
+                fondo.style.color = "black"
+                for(const element of txt) { 
+            
+                    element.style.color = 'black';
+                }
+
+            fondo.classList.add('light_mode');  // agrego clase
+        }else{
+            btn.style.backgroundColor = 'white';
+            btn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="5" fill="currentColor"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42" stroke="currentColor" stroke-width="2" stroke-linecap="round"/></svg>';
+            btn.style.color = "#282828"
+            fondo.style.background = '##212121';
+            for(const element of txt) {
+                element.style.color = 'white';
+            }
+            fondo.classList.remove('light_mode');  // agrego clase
+
+        }
+
     case 'myprofile.html':
         if (modo == 'claro'){
             fondo.classList.add('light_mode');  // agrego clase
