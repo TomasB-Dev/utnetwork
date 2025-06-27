02e088e6e73d8a2a8e053fd5fd768b94cd56827d
Agustín Sosa <106622274+Agus-Sosa@users.noreply.github.com>
2025-06-27 18:20:12 -0300

    actualizacion de descripcion

--- static/js/myprofile.js
+++ static/js/myprofile.js
@@ -15,9 +15,31 @@ const verifyComment=(e)=> {
         id_error.style.display='none'
         update_description_form.submit()
     }
+}
 
+
+const btn_edit_description = document.getElementById('button_edit_description')
+
+
+const edit_description = ()=> {
+    const description = document.getElementById('description_container');
+    const container_edit_description = document.getElementById("container_update_desc");
+
+
+    description.classList.add('hidden');
+    container_edit_description.classList.remove('hidden');
+    
 
 }
+
+
+
+btn_edit_description.addEventListener("click", edit_description)
+
+
+
+
+
 
 
 add_description.addEventListener('submit', (e)=> verifyComment(e));
